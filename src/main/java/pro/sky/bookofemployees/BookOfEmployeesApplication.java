e063dcfe04810f01fb1fcbd156feeb9e090e0f43
Alexey Kolodin <alekko-sp@yandex.ru>
2023-04-04 15:43:49 +0300

    it's empty here

---
+++ src/main/java/pro/sky/bookofemployees/BookOfEmployeesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.bookofemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOfEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOfEmployeesApplication.class, args);
+	}
+
+}
